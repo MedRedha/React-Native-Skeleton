3edb2a1e0f054cb5be92d7325c5d6c8ee042b9c7
Nassim Amokrane <na66im@gmail.com>
2019-10-27 00:44:08 +0100

    example reducer/action/saga

--- src/sagas.js
+++ src/sagas.js
@@ -1,8 +1,6 @@
 import {all, fork} from 'redux-saga/effects';
 import {watchAuth} from './containers/Auth/Auth.sagas';
 
-//<-- IMPORT MODULE SAGA -->
-
 export default function* rootSaga() {
   yield all([
     fork(watchAuth),
