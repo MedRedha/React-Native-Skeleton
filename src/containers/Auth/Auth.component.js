40b0f598ca38c8db40791ed3c5c72dbe59a28e11
Med Redha <redha.dirac@gmail.com>
2019-10-27 02:25:44 +0100

    Prettier code syntax fix

--- src/containers/Auth/Auth.component.js
+++ src/containers/Auth/Auth.component.js
@@ -1,18 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, {useEffect} from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, Text} from 'react-native';
 
 export const Auth: () => React$Node = props => {
   useEffect(() => {
     props.changeEmail();
-  }, []);
+  }, [props]);
   return (
     <View>
       <Text>Text</Text>
